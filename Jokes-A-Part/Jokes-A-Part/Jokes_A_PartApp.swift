94f6137e385661e6ec586e40267294c3559c2b3c
Omkar Vijay Bagade <omkarbagade16@gmail.com>
2025-11-25 23:29:26 +0530

    Create initial Xcode project folder structure
    This is a folder structure which will update as the project moves futher

---
+++ Jokes-A-Part/Jokes-A-Part/Jokes_A_PartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_A_PartApp.swift
+//  Jokes-A-Part
+//
+//  Created by Omkar Vijay Bagade on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_A_PartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
