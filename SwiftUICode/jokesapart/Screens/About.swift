f24272e4c9bb9886686d3bf426ffd43c7e96fe65
Omkar Vijay Bagade <omkarbagade16@gmail.com>
2025-11-26 00:54:54 +0530

    Revamped the entire folder structure

---
+++ SwiftUICode/jokesapart/Screens/About.swift
@@ -0,0 +1,18 @@
+//
+//  About.swift
+//  jokesapart
+//
+//  Created by Omkar Vijay Bagade on 26/11/25.
+//
+
+import SwiftUI
+
+struct About: View {
+    var body: some View {
+        Text("About")
+    }
+}
+
+#Preview {
+    About()
+}
---
+++ SwiftUICode/jokesapart/Screens/Favourites.swift
@@ -0,0 +1,18 @@
+//
+//  Favourites.swift
+//  jokesapart
+//
+//  Created by Omkar Vijay Bagade on 26/11/25.
+//
+
+import SwiftUI
+
+struct Favourites: View {
+    var body: some View {
+        Text("Favourites")
+    }
+}
+
+#Preview {
+    Favourites()
+}
