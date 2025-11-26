ff02a661a23ebdfe2c0076bf92f26e984de8cfb0
Omkar Vijay Bagade <omkarbagade16@gmail.com>
2025-11-26 10:05:49 +0530

    create onboarding screen
    Did some changes in the color assets and color extension along with static UI of onboarding screen

--- SwiftUICode/jokesapart/Screens/Onboarding.swift
+++ SwiftUICode/jokesapart/Screens/Onboarding.swift
@@ -9,7 +9,79 @@ import SwiftUI
 
 struct Onboarding: View {
     var body: some View {
-        Text("onboarding")
+        ZStack {
+            Color.theme.white.ignoresSafeArea()
+            VStack {
+                //Hero Text
+                VStack {
+                    Text("Jokes")
+                    Text("APart")
+                }
+                .foregroundStyle(Color.theme.darkGreen)
+                .font(Font.custom("IrishGrover-Regular", size: 80))
+                .offset(x: -20)
+
+                //Hero image
+                Image("Onboardingimage")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 550, height: 550)
+                    .padding(.top, -196)
+
+                VStack(spacing: 5) {
+                    Text("Welcome to The Jokes APart")
+                        .foregroundStyle(Color.theme.darkGreen)
+                        .font(Font.custom("IrishGrover-Regular", size: 30))
+                        .multilineTextAlignment(.center)
+
+                    Text("Let’s  have fun with jokes .")
+                        .foregroundStyle(Color.theme.darkGreen)
+                        .font(Font.custom("InriaSans-Regular", size: 20))
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, -100)
+
+                Spacer()
+
+                //Button
+                Button(action: {}) {
+                    HStack {
+                        Text("Lets Go")
+                            .foregroundStyle(Color.theme.white)
+                            .frame(width: 148, height: 35)
+                            .font(Font.custom("IrishGrover-Regular", size: 30))
+                            
+                        Image(systemName: "arrow.right")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:30,height:30)
+                            .foregroundStyle(Color.theme.white)
+                    }
+                    .frame(width: 200, height: 50)
+                    .background(Color.theme.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 50))
+                }
+                .padding(.top,-50)
+                .shadow(color: Color.darkGreen ,radius: 10,y:4)
+                
+                Spacer()
+                
+                VStack(spacing: 5) {
+                    HStack {
+                        Text("Having Issues?")
+                        Text("Get Help")
+                            .foregroundStyle(Color.theme.green)
+                    }
+                    HStack {
+                        Text("Already have an Account?")
+                        Text("Sign In")
+                            .foregroundStyle(Color.theme.green)
+                    }
+                    
+                }.font(Font.custom("InriaSans-Regular", size: 20))
+                
+            }
+        }
     }
 }
 
